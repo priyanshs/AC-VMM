8a74e1000d2be9d84a4c186f8fa57ab5ff6ca7c7
Kshitij <kshitij.kotasthane@gmail.com>
2022-10-25 12:19:14 +0530

    compute similarity and append data to JSON file

--- static_checker/src/main.rs
+++ static_checker/src/main.rs
@@ -1,8 +1,9 @@
 use ignore::Walk;
-use rayon::prelude::*;
+// use rayon::prelude::*;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
-// use std::process::Command;
+use serde_json;
+use std::io::Write;
 
 mod lib;
 mod preprocessors;
@@ -10,25 +11,8 @@ mod preprocessors;
 #[derive(StructOpt, Debug)]
 #[structopt(set_term_width = 80)]
 struct Cli {
-    /// Only show files with specified similarity percentage.
-    // #[structopt(short, long, default_value = "80")]
-    // similarity: f64,
-
-    /// File preprocessor to use.
-    ///
-    /// * "asm": x86 GAS assembly
-    /// * "c": C programming language
-    /// * "text": Basic text preprocessing
-    /// * "none": Disable preprocessing {n}
-    // #[structopt(
-    //     short,
-    //     long,
-    //     default_value = "asm",
-    //     possible_values = &["asm", "c", "text", "none"],
-    //     verbatim_doc_comment,
-    // )]
-    // preprocessor: String,
-
+    #[structopt(default_value="./src/test/orig.rs")]
+    source: PathBuf,
     /// Source file to check for plagiarism.
     // source: PathBuf,
     /// Targets to compare against the source file.
@@ -40,17 +24,37 @@ struct Cli {
 fn main() {
     let cli = Cli::from_args();
     let targets = &walk_directories(&cli.targets);
-    let source = Path::new("./src/test/orig.rs");
-    dbg!(targets);
-    // TODO: solve preprocessor issue as its needed in both lib and main
+    // write to this file as well as DB
+    let output_file = "results.json";
+    // dbg!(&cli.source);
+    // dbg!(targets);
+
+    // create a new Box for the CPreprocessor and use that wih Rust code
     let preprocessor = Some(Box::new(lib::preprocessors::CPreprocessor::new()));
 
-    // TODO: uncomment later to detect all files and store similarity score
-    for result in lib::detect(&source, &targets, preprocessor.as_deref()) {
+    for result in lib::detect(&cli.source, &targets, preprocessor.as_deref()) {
         match result {
-            // set to 50% similarity for convenience
-            Ok((target, score)) if score >= 0. / 100. => {
-                println!("\"{}\" : {:.2}%", target.display(), score * 100.);
+            // set similarity threshold to 1% for convenience
+            // should output all files in most cases
+            Ok((target, score)) if score >= 1. / 100. => {
+                // DB handling goes here
+                // temporarily write to a JSON file
+                let data = serde_json::json!({
+                    "file": target,
+                    "similarity": score * 100.
+                });
+                // debug output to check if preprocessor works
+                // println!("\"{}\" : {:.2}%", target.display(), score * 100.);
+                // append to file rather than replace contents
+                let mut file_ref = std::fs::OpenOptions::new().append(true).open(output_file).unwrap();
+
+                file_ref.write_all(serde_json::to_string_pretty(&data).unwrap().as_bytes()).expect("appending data failed");
+
+                // writes replacing all content, so only last entry is present at the end
+                // std::fs::write(
+                //     output_file,
+                //     serde_json::to_string_pretty(&data).unwrap()
+                // );
             }
             Err(e) => {
                 eprintln!("{}", e);
@@ -59,16 +63,6 @@ fn main() {
         }
     }
 }
-
-// fn get_preprocessor(pp: &str) -> Option<Box<dyn preprocessors::Preprocessor + Sync>> {
-//     match pp {
-//         "asm" => Some(Box::new(preprocessors::AsmPreprocessor::new())),
-//         "c" => Some(Box::new(preprocessors::CPreprocessor::new())),
-//         "text" => Some(Box::new(preprocessors::TextPreprocessor::new())),
-//         "none" => None,
-//         _ => unreachable!(),
-//     }
-// }
 
 fn walk_directories<P: AsRef<Path>>(paths: &[P]) -> Vec<PathBuf> {
     paths
---
+++ static_checker/src/test/targets/2.rs
@@ -0,0 +1,7 @@
+fn add(a: u32, b: u32) -> u32 {
+    a+b
+}
+
+fn main(){
+    println!("this file has an extra function!!!");
+}
