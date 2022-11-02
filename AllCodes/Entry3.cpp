179759f37a360bb85eede2b2d6e3a1625798cfd8
Arka Mandal <arkatigps14@gmail.com>
2022-11-02 19:22:24 +0530

    codes merged

---
+++ AllCodes/Entry3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+int main() {
+    std::cout << "Hello World!";
+    int i=1;
+    int j=101;
+    int sum=i+j;
+    for(int i=0;i<4;i++){
+        sum+=1;
+    }
+    return 0;
+}
---
+++ AllCodes/Entry4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+int main() {
+    std::cout << "Hello World!";
+    int i=1;
+    int j=99;
+    int sum=i+j;
+    for(int i=0;i<6;i++){
+        sum+=1;
+    }
+    return 0;
+}
