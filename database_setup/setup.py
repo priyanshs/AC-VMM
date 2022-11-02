97534f8828867ff8db20a8f2011c3b5ab98b524b
Priyansh <priyanshsingh@outlook.com>
2022-11-02 17:02:34 +0530

    creates the database and table schema

--- database_setup/setup.py
+++ database_setup/setup.py
@@ -1,3 +1,8 @@
+"""
+The following code connects to the local server and creates the database and schema
+"""
+
+
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -9,13 +14,36 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute("USE mydatabase")
 mydb.database = "mydatabase"
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE customers (VARCHAR(255), address VARCHAR(255))")
-# mycursor.execute("SHOW DATABASES")
-mycursor.execute("SHOW TABLES")
 
+table_schema ="""CREATE TABLE `grading` (
+	`s_id` INT,
+	`vm_id` INT,
+	`assign_id` INT,
+	`time_vm` INT,
+	`no_launches` INT,
+	`activity_1` INT,
+	`activity_2` INT,
+	`activity_3` INT,
+	`static_dist` INT,
+	`grading_time` INT,
+	`dynamic_dist` INT,
+	`cheat_label` INT,
+	`marks` INT,        
+ 	PRIMARY KEY (`s_id`,`assign_id`)
+);"""
 
-for x in mycursor:
-  print(x) 
+mycursor.execute(table_schema)
+# # mycursor.execute("SHOW DATABASES")
+# mycursor.execute("SHOW TABLES")
+
+# for x in mycursor:
+#   print(x) 
+
+#   mycursor.execute("DESCRIBE grading")
+
+# for x in mycursor:
+#   print(x) 
