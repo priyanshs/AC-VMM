f043d135c56995beedb828b5c965ffab6f0869cd
Arka Mandal <arkatigps14@gmail.com>
2022-11-05 13:48:25 +0530

    py arguments supported

--- AllCodes/pyTime.rs
+++ AllCodes/pyTime.rs
@@ -7,12 +7,12 @@ use std::env;
 use std::fs::File;
 use std::time::Instant;
 use std::io::{self, prelude::*, BufReader};
-pub fn timeCheckPy(st:String){
+pub fn timeCheckPy(st:String,args:String){
     let now = Instant::now();
     let mut compile = Command::new("sh");
-    let mut comp="python ".to_owned()+&st+".py";
+    let mut comp="python3 ".to_owned()+&st+".py "+&args;
     compile.arg("-c").arg(comp);
-    let x=compile.output().expect("failed to execute process").stdout;
+    let x=compile.output().expect("failed to execute process");
     let elapsed = now.elapsed().as_nanos();
     let mut file = OpenOptions::new()
         .write(true)
@@ -26,7 +26,15 @@ fn main() {
     let mut file = File::create("time");
     let file = File::open("submissions").unwrap();
     let reader = BufReader::new(file);
+    let mut firstLine=true;
+    let mut args:String="a".to_string();
     for line in reader.lines() {
-        timeCheckPy(line.as_ref().unwrap().clone());
+        if firstLine{
+            firstLine=false;
+            args=line.as_ref().unwrap().clone();
+        }
+        else{
+            timeCheckPy(line.as_ref().unwrap().clone(),args.clone());
+        }
     }
 }
